a679061d5d0c4262a324d7df26f0a9a63e30c65b
sirrosh <noone@nowhere.com>
2020-05-22 22:15:25 +0200

    First sensible commit

--- main.asm
+++ main.asm
@@ -1,21 +1,20 @@
-	list	p=16f648a
-	radix dec
+list	p=16f648a
+radix dec
 #include "p16f648a.inc"
-;--------------------------------------------------------
-; config word(s)
-;--------------------------------------------------------
-	__config 0x3f78
+;__config 0x3f78
+__CONFIG   _CP_OFF & _CPD_OFF & _WDT_OFF & _PWRTE_ON & _INTRC_OSC_NOCLKOUT & _MCLRE_OFF & _LVP_OFF
+
+#define SINE_TABLE_ADDRESS_H	0x02
+#define SINE_TABLE_ADDRESS	0x0200
+
+SINE_TABLE	CODE	SINE_TABLE_ADDRESS
 #include sine.inc
 
-    CBLOCK 0x20
+    CBLOCK 0x70 ; 0x70-0x7F -- 16 bytes of bank independent memory block
 i_cycle
 W_temp
+W_swap
 STATUS_temp
-TH_h
-TH_l
-T_h
-T_l
-wMode
     ENDC
 
 
@@ -29,13 +28,32 @@ RES_VECT    CODE    0x0000          ; processor reset vector
 INT_VECT    CODE    0x0004          ; interrupt vector
     GOTO    INTERRUPT
 
+TABLEREAD   CODE    0x0006          ; table read routine, should be at fixed address
+TBLRD
+    MOVWF   W_swap
+    MOVLW   SINE_TABLE_ADDRESS_H
+    MOVWF   PCLATH
+    MOVFW   W_swap
+    MOVWF   PCL                     ; a table would start at the segment boundary
+    RETURN                          ; this should never been executed
 
 MAIN_PROG   CODE                    ; let linker place main program
 
 START
+    banksel TRISB
+    CLRF   TRISB            ; port B to output
+    banksel PORTB
+    CLRF   i_cycle
+MAIN_LOOP
+    MOVFW  i_cycle
+    CALL   TBLRD
+    MOVWF  PORTB
+    INCF   i_cycle
+    GOTO   MAIN_LOOP
 
 
 
+;--- ISR stub
 INTERRUPT
     MOVWF   W_temp          ; copy W to temp register, could be in either bank
     SWAPF   STATUS, W       ; swap status to be saved into W
